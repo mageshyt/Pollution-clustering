10a60bb363198b806129b054d132a3553cf6afcc
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-12-20 09:17:10 +0530

    Add data preprocessing and clustering code

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import plotly.express as px
 
+# 1. Load the data
 df=pd.read_csv('./data/train_cleaned.csv')
 
-
+# 2. get the features
 X=df.drop(['Name of station','DBU Class'],axis=1)
 
+# 3. standardize the data
 scaler=StandardScaler()
 
-# standardize the data
 X=scaler.fit_transform(X)
 
+# 4. find the optimal number of clusters    
 kmeans=KMeans(n_clusters=3,random_state=42)
 
 kmeans.fit(X)
 
 
+# 5. predict the clusters
 df['cluster-3']=kmeans.fit_predict(X)
 
+# 6. plot the clusters
 px.scatter(df,x='D.O mg/L',y='pH',color='cluster-3')
+
+
 
 
 ## if we find to find the clustring for specific columns 
